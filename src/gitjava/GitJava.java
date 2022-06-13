9fd912c6576fe0dbc13a01fac52b814009edf026
manana <example@gmail.com>
2022-06-13 14:20:11 +0200

    Añadimos el dia 15

--- src/gitjava/GitJava.java
+++ src/gitjava/GitJava.java
@@ -18,6 +18,7 @@ public class GitJava {
         // TODO code application logic here
         System.out.println("Hoy es Lunes 13");
         System.out.println("Mañana es 14");
+        System.out.println("Pasado es 15");
         
     }
     
